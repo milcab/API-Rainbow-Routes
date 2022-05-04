751f76f6a70e5b8d452dfa82e9cc88445140e7e8
milcab <milcarodriguez23@gmail.com>
2022-05-04 15:01:10 -0400

    setting up the API routes

--- index.ts
+++ index.ts
@@ -1,8 +1,9 @@
 // Get the environment variables
 require('dotenv').config()
 
+
 // Require needed node modules
-const express = require('express')
+import express from 'express'
 
 // Initialize your application by calling the function returned by the express module
 const app = express()
@@ -23,16 +24,20 @@ app.get('/', function (req, res) {
 
 // Color Page
 app.get('/:color', function (req, res) {
-    let myColor = req.params.color
-    res.send(`
-        <body style="margin: 0;">
-            <div style="border: 1px solid black; height: 10vh; background-color: ${myColor};">
-                <h2 style="text-align: center;">NAV BAR</h2>
-            </div>
-            <h1 style="color: ${myColor};">${myColor.charAt(0).toUpperCase() + myColor.slice(1)} Page</h1>
-        </body>
-    `)
+    let myColor: unknown = req.params.color
+    if (typeof(myColor) === 'string'){
+        res.send(`
+            <body style="margin: 0;">
+                <div style="border: 1px solid black; height: 10vh; background-color: ${myColor};">
+                    <h2 style="text-align: center;">NAV BAR</h2>
+                </div>
+                <h1 style="color: ${myColor};">${myColor.charAt(0).toUpperCase() + myColor.slice(1)} Page</h1>
+            </body>
+        `)
+    }
 })
+
+
 
 
 // Listen to a port number defined by a local environment variable
